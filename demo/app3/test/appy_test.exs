938ca3f7bb0bc0a9ade448ab457aedcbe7ca918f
Paul Engel <pm_engel@icloud.com>
2018-11-24 15:34:27 +0100

    Replace demo apps with (a single) demo/app

--- demo/app3/test/appy_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule AppyTest do
-  use ExUnit.Case
-  doctest Appy
-
-  test "greets the world" do
-    assert Appy.hello() == :world
-  end
-end
--- demo/tmux.sh
+++ demo/tmux.sh
@@ -3,18 +3,18 @@
 session=clustor
 root="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null && pwd)"
 
-tmux start-server
+cd $root/app
 
-cd $root/app1
+tmux start-server
 tmux new-session -d -s "$session" -n "app1"
 
 for i in {2..3}; do
-  cd $root/app$i
   tmux new-window -t "$session:$i" -n "app$i"
 done
 
 for i in {1..3}; do
-  tmux send-keys -t "$session:$i" "mix deps.get; iex -S mix" C-m
+  sleep 0.2
+  tmux send-keys -t "$session:$i" "mix deps.get; source ../env/app$i; iex -S mix" C-m
 done
 
 tmux attach -t $session
